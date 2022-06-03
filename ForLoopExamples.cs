118035878873f7b81f6ece2632d19ee32eac6243
furkancetinalp <99509540+furkancetinalp@users.noreply.github.com>
2022-06-03 20:49:10 +0300

    Update ForLoopExamples.cs

--- ForLoopExamples.cs
+++ ForLoopExamples.cs
@@ -1,19 +1,10 @@
-using System.CodeDom.Compiler;
-using System.Collections.Generic;
-using System.Collections;
-using System.ComponentModel;
-using System.Diagnostics.CodeAnalysis;
-using System.Globalization;
-using System.IO;
-using System.Linq;
-using System.Reflection;
-using System.Runtime.Serialization;
-using System.Text.RegularExpressions;
-using System.Text;
 using System;
 
-class Result
+namespace app3
 {
+    class Result
+{
+
     public static void staircase(int n)
     {
         for(int i=0;i<n;i++){
@@ -25,23 +16,25 @@ class Result
                 Console.Write("#");
             }
             Console.WriteLine();
-        }    
-    }
-}
-
-class Solution
-{
-    }
-    
-    public static void Main(string[] args)
-    {
-        int n = Convert.ToInt32(Console.ReadLine().Trim());
-        for(int i=1;i<=10;i++){
-            Console.WriteLine("{0} x {1} = {2}",n,i,n*i);
         }
-        
-        int n = Convert.ToInt32(Console.ReadLine().Trim());
+      
+    }
 
-        Result.staircase(n);
+}
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int n = Convert.ToInt32(Console.ReadLine().Trim());
+            for(int i=1;i<=10;i++){
+                Console.WriteLine("{0} x {1} = {2}",n,i,n*i);
+            }
+
+            int m = Convert.ToInt32(Console.ReadLine().Trim());
+
+            Result.staircase(m);
+        }
     }
 }
+
