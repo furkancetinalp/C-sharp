5f8b57f150c6676f9413d1004cb58e4324292378
furkancetinalp <99509540+furkancetinalp@users.noreply.github.com>
2022-06-03 21:06:02 +0300

    Create whileLoop.cs

---
+++ whileLoop.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace app3
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            char character = 'a';
+            while (character<='z'){
+                System.Console.Write(character+",");
+                character++;
+            }
+
+            
+        }
+    }
+}
